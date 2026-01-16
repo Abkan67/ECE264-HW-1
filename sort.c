418f5772eb25264d605e26e9e22d50f15b04319b
Abkan67 <78571678+Abkan67@users.noreply.github.com>
2026-01-16 11:53:33 -0500

    Add files via upload

---
+++ sort.c
@@ -0,0 +1,16 @@
+/* MODIFY this file */
+
+#include "sort.h"
+
+/*
+	Takes an array and returns a sorted array.
+	Parameters:
+	arr: 
+*/
+
+void ssort(int * arr, int size) {
+
+
+	/* For step 3, fill this in to perform a selection sort
+	   For step 4, add conditional compilation flags to perform an ascending selection sort instead */
+}
